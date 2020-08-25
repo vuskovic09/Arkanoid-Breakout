09a3f54c369afecaf952a726bb7de3ef23b55772
Filip Vušković <filip.vuskovic09@gmail.com>
2020-08-25 17:22:00 +0200

    ADD: Ball spawn

---
+++ Assets/Scripts/BallsManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallsManager : MonoBehaviour
+{
+
+    #region Singleton
+
+    private static BallsManager _instance;
+
+    public static BallsManager Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    #endregion
+
+    [SerializeField]
+    private Ball ballPrefab;
+
+    private Ball initialBall;
+
+    private Rigidbody2D initialBallRb;
+
+    public List<Ball> Balls { get; set; }
+
+    private void Start()
+    {
+        InitBall();
+    }
+
+    private void InitBall()
+    {
+        Vector3 paddlePosition = Paddle.Instance.gameObject.transform.position;
+        Vector3 startingPosition = new Vector3(paddlePosition.x, paddlePosition.y + .27f, 0); //get it from the paddle
+        initialBall = Instantiate(ballPrefab, startingPosition, Quaternion.identity);
+        initialBallRb = initialBall.GetComponent<Rigidbody2D>();
+
+        this.Balls = new List<Ball>
+        {
+            initialBall
+        };
+    }
+
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,17 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Paddle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Singleton
+
+    private static Paddle _instance;
+
+    public static Paddle Instance => _instance;
+
+    private void Awake()
     {
-        
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    private Camera mainCamera;
+    private float paddleInitialY;
+    private float defaultPaddleWidthInPixels = 200;
+    private SpriteRenderer sr;
+    private float defaultLeftClamp = 135;
+    private float defaultRightClamp = 410;
+
+    private void Start()
     {
-        
+        mainCamera = FindObjectOfType<Camera>();
+        paddleInitialY = this.transform.position.y;
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate() {
+        PaddleMovement();
+    }
+
+    private void PaddleMovement()
+    {
+        float paddleShift = (defaultPaddleWidthInPixels - ((defaultPaddleWidthInPixels / 2) * this.sr.size.x)) / 2;
+        float leftClamp = defaultLeftClamp - paddleShift;
+        float rightClamp = defaultRightClamp + paddleShift;
+        float mousePositionPixels = Mathf.Clamp(Input.mousePosition.x, leftClamp, rightClamp);
+        float mousePositionWorldX = mainCamera.ScreenToWorldPoint(new Vector3(mousePositionPixels, 0, 0)).x;
+        this.transform.position = new Vector3(mousePositionWorldX, paddleInitialY, 0);
     }
 }

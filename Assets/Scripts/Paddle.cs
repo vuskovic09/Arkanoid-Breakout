2bbe1f656f9e1ba06623f325df3ba794d5187ed7
Filip Vušković <filip.vuskovic09@gmail.com>
2020-08-25 17:47:16 +0200

    ADD: Ball movement (sideways)

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -51,4 +51,29 @@ public class Paddle : MonoBehaviour
         float mousePositionWorldX = mainCamera.ScreenToWorldPoint(new Vector3(mousePositionPixels, 0, 0)).x;
         this.transform.position = new Vector3(mousePositionWorldX, paddleInitialY, 0);
     }
+
+
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "Ball")
+        {
+            Rigidbody2D ballRb = coll.gameObject.GetComponent<Rigidbody2D>();
+            Vector3 hitPoint = coll.contacts[0].point;
+            Vector3 paddleCenter = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y);
+
+            ballRb.velocity = Vector2.zero;
+
+            float difference = paddleCenter.x - hitPoint.x;
+
+            if (hitPoint.x < paddleCenter.x)
+            {
+                ballRb.AddForce(new Vector2(-(Mathf.Abs(difference * 200)), BallsManager.Instance.initialBallSpeed));
+            } else
+            {
+                ballRb.AddForce(new Vector2(Mathf.Abs(difference * 200), BallsManager.Instance.initialBallSpeed));
+            }
+
+        }
+    }
 }

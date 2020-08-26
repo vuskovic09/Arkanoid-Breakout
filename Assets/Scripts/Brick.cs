a4f6a7b619a850ecb4117524861a6f877b4535c0
Filip Vušković <filip.vuskovic09@gmail.com>
2020-08-26 19:30:54 +0200

    ADD: brick image + particle system death effect

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public int Hitpoints = 1;
+    public ParticleSystem DestroyEffect;
+
+    public static event Action<Brick> OnBrickDestruction;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+        ApplyCollisionLogic(ball);
+    }
+
+    private void ApplyCollisionLogic(Ball ball)
+    {
+        this.Hitpoints--;
+
+        if(this.Hitpoints <= 0)
+        {
+            OnBrickDestruction?.Invoke(this);
+            //SpawnDestroyEffect();
+            Destroy(this.gameObject);
+        } 
+        else 
+        {
+            //sprite change
+        }
+    }
+}

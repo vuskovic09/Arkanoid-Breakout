29425a118fc57ecfef075c5dbacb04884d7815ce
Filip Vušković <filip.vuskovic09@gmail.com>
2020-08-27 18:11:11 +0200

    ADD: extend and shrink paddle collectibles

---
+++ Assets/Scripts/Collectibles/ExtendOrShrink.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtendOrShrink : Collectible
+{
+    public float NewWidth = 2.5f;
+
+    protected override void ApplyEffect()
+    {
+        if (Paddle.Instance != null & !Paddle.Instance.PaddleIsTransforming)
+        {
+            Paddle.Instance.StartWidthAnimation(NewWidth);
+        }
+    }
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +10,8 @@ public class Paddle : MonoBehaviour
     private static Paddle _instance;
 
     public static Paddle Instance => _instance;
+
+    public bool PaddleIsTransforming { get; set; }
 
     private void Awake()
     {
@@ -21,7 +24,6 @@ public class Paddle : MonoBehaviour
             _instance = this;
         }
     }
-
     #endregion
 
     private Camera mainCamera;
@@ -30,12 +32,18 @@ public class Paddle : MonoBehaviour
     private SpriteRenderer sr;
     private float defaultLeftClamp = 135;
     private float defaultRightClamp = 410;
+    private BoxCollider2D boxCol;
+
+    public float extendShrinkDuration = 10;
+    public float paddleWidth = 2;
+    public float paddleHeight = 0.28f;
 
     private void Start()
     {
         mainCamera = FindObjectOfType<Camera>();
         paddleInitialY = this.transform.position.y;
         sr = GetComponent<SpriteRenderer>();
+        boxCol = GetComponent<BoxCollider2D>();
     }
 
     private void FixedUpdate() {
@@ -51,8 +59,6 @@ public class Paddle : MonoBehaviour
         float mousePositionWorldX = mainCamera.ScreenToWorldPoint(new Vector3(mousePositionPixels, 0, 0)).x;
         this.transform.position = new Vector3(mousePositionWorldX, paddleInitialY, 0);
     }
-
-
 
     private void OnCollisionEnter2D(Collision2D coll)
     {
@@ -75,5 +81,47 @@ public class Paddle : MonoBehaviour
             }
 
         }
+    }
+
+    public void StartWidthAnimation(float newWidth)
+    {
+        StartCoroutine(AnimatePaddleWidth(newWidth));
+    }
+
+    public IEnumerator AnimatePaddleWidth(float width)
+    {
+        this.PaddleIsTransforming = true;
+        this.StartCoroutine(ResetPaddleWidthAfterTime(this.extendShrinkDuration));
+
+        if (width > this.sr.size.x)
+        {
+            float currentWidth = this.sr.size.x;
+            while (currentWidth < width)
+            {
+                currentWidth += Time.deltaTime * 2;
+                this.sr.size = new Vector2(currentWidth, paddleHeight);
+                boxCol.size = new Vector2(currentWidth, paddleHeight);
+                yield return null;
+            }
+        }
+        else
+        {
+            float currentWidth = this.sr.size.x;
+            while (currentWidth > width)
+            {
+                currentWidth -= Time.deltaTime * 2;
+                this.sr.size = new Vector2(currentWidth, paddleHeight);
+                boxCol.size = new Vector2(currentWidth, paddleHeight);
+                yield return null;
+            }
+        }
+
+        this.PaddleIsTransforming = false;
+    }
+
+    private IEnumerator ResetPaddleWidthAfterTime(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        this.StartWidthAnimation(this.paddleWidth);
     }
 }
